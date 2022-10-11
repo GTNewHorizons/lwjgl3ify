d28da519812adf6d9abcaf8f970dd7db8a300c80
Raven Szewczyk <git@eigenraven.me>
2022-10-11 20:06:38 +0100

    Template

---
+++ src/main/java/me/eigenraven/lwjgl3ify/Tags.java
@@ -0,0 +1,12 @@
+package me.eigenraven.lwjgl3ify;
+
+// Use this class for Strings only. Do not import any classes here. It will lead to issues with Mixins if in use!
+
+public class Tags {
+
+    // GRADLETOKEN_* will be replaced by your configuration values at build time
+    public static final String MODID = "lwjgl3ify";
+    public static final String MODNAME = "lwjgl3ify";
+    public static final String VERSION = "GRADLETOKEN_VERSION";
+    public static final String GROUPNAME = "GRADLETOKEN_GROUPNAME";
+}
