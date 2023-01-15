40fbeff61846d0a425e47e846c880c2aee277e7e
Raven Szewczyk <git@eigenraven.me>
2023-01-15 12:07:55 +0000

    Use MemoryUtil.memByteBuffer instead of copying

--- src/main/java/me/eigenraven/lwjgl3ify/BufferCasts.java
+++ src/main/java/me/eigenraven/lwjgl3ify/BufferCasts.java
@@ -1,82 +1,46 @@
 package me.eigenraven.lwjgl3ify;
 
 import java.nio.*;
-import java.nio.charset.StandardCharsets;
+import org.lwjgl.system.MemoryUtil;
 
 public class BufferCasts {
     public static ByteBuffer toByteBuffer(CharBuffer buffer) {
-        ByteBuffer newBuffer = ByteBuffer.allocateDirect(buffer.capacity() * 4).order(ByteOrder.nativeOrder());
-        newBuffer.asCharBuffer().put(buffer);
-        return newBuffer;
+        return MemoryUtil.memByteBuffer(buffer);
     }
 
     public static ByteBuffer toByteBuffer(ShortBuffer buffer) {
-        ByteBuffer newBuffer = ByteBuffer.allocateDirect(buffer.capacity() * 4).order(ByteOrder.nativeOrder());
-        newBuffer.asShortBuffer().put(buffer);
-        return newBuffer;
+        return MemoryUtil.memByteBuffer(buffer);
     }
 
     public static ByteBuffer toByteBuffer(IntBuffer buffer) {
-        ByteBuffer newBuffer = ByteBuffer.allocateDirect(buffer.capacity() * 4).order(ByteOrder.nativeOrder());
-        newBuffer.asIntBuffer().put(buffer);
-        return newBuffer;
+        return MemoryUtil.memByteBuffer(buffer);
     }
 
     public static ByteBuffer toByteBuffer(LongBuffer buffer) {
-        ByteBuffer newBuffer = ByteBuffer.allocateDirect(buffer.capacity() * 4).order(ByteOrder.nativeOrder());
-        newBuffer.asLongBuffer().put(buffer);
-        return newBuffer;
+        return MemoryUtil.memByteBuffer(buffer);
     }
 
     public static ByteBuffer toByteBuffer(FloatBuffer buffer) {
-        ByteBuffer newBuffer = ByteBuffer.allocateDirect(buffer.capacity() * 4).order(ByteOrder.nativeOrder());
-        newBuffer.asFloatBuffer().put(buffer);
-        return newBuffer;
+        return MemoryUtil.memByteBuffer(buffer);
     }
 
     public static ByteBuffer toByteBuffer(DoubleBuffer buffer) {
-        ByteBuffer newBuffer = ByteBuffer.allocateDirect(buffer.capacity() * 4).order(ByteOrder.nativeOrder());
-        newBuffer.asDoubleBuffer().put(buffer);
-        return newBuffer;
+        return MemoryUtil.memByteBuffer(buffer);
     }
 
-    public static void updateBuffer(CharBuffer destination, ByteBuffer source) {
-        destination.clear();
-        destination.put(source.asCharBuffer());
-    }
+    public static void updateBuffer(CharBuffer destination, ByteBuffer source) {}
 
-    public static void updateBuffer(ShortBuffer destination, ByteBuffer source) {
-        destination.clear();
-        destination.put(source.asShortBuffer());
-    }
+    public static void updateBuffer(ShortBuffer destination, ByteBuffer source) {}
 
-    public static void updateBuffer(IntBuffer destination, ByteBuffer source) {
-        destination.clear();
-        destination.put(source.asIntBuffer());
-    }
+    public static void updateBuffer(IntBuffer destination, ByteBuffer source) {}
 
-    public static void updateBuffer(LongBuffer destination, ByteBuffer source) {
-        destination.clear();
-        destination.put(source.asLongBuffer());
-    }
+    public static void updateBuffer(LongBuffer destination, ByteBuffer source) {}
 
-    public static void updateBuffer(FloatBuffer destination, ByteBuffer source) {
-        destination.clear();
-        destination.put(source.asFloatBuffer());
-    }
+    public static void updateBuffer(FloatBuffer destination, ByteBuffer source) {}
 
-    public static void updateBuffer(DoubleBuffer destination, ByteBuffer source) {
-        destination.clear();
-        destination.put(source.asDoubleBuffer());
-    }
+    public static void updateBuffer(DoubleBuffer destination, ByteBuffer source) {}
 
     public static CharSequence bufferToCharSeq(ByteBuffer buffer) {
-        if (buffer.hasArray()) {
-            return new String(buffer.array(), StandardCharsets.UTF_8);
-        } else {
-            byte[] tmp = new byte[buffer.remaining()];
-            buffer.get(tmp);
-            return new String(tmp, StandardCharsets.UTF_8);
-        }
+        return MemoryUtil.memUTF8(buffer);
     }
 }
