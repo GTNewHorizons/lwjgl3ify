23a37521b0767cd430f82ef38552c81c59e32ad5
Raven Szewczyk <git@eigenraven.me>
2023-01-18 15:13:33 +0000

    Add a CoFH compat hook

--- src/main/java/me/eigenraven/lwjgl3ify/CommonProxy.java
+++ src/main/java/me/eigenraven/lwjgl3ify/CommonProxy.java
@@ -1,5 +1,23 @@
 package me.eigenraven.lwjgl3ify;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
 public class CommonProxy {
     public void registerF3Handler() {}
+
+    public void runCompatHooks() {
+        // Fix crash with OpenComputers/COFHCore due to a class extending OpenALException
+        try {
+            Class<?> cofhATTransformer = Class.forName("cofh.asm.CoFHAccessTransformer");
+            Field superClassesField = cofhATTransformer.getDeclaredField("superClasses");
+            superClassesField.setAccessible(true);
+            HashMap<String, String> superClasses = (HashMap<String, String>) superClassesField.get(null);
+            superClasses.put("org/lwjgl/openal/OpenALException", "java/lang/RuntimeException");
+            superClasses.put("org/lwjglx/openal/OpenALException", "java/lang/RuntimeException");
+            Lwjgl3ify.LOG.info("Installed CoFH AT compatibility");
+        } catch (ReflectiveOperationException e) {
+            // ignore - cofh not present
+        }
+    }
 }
