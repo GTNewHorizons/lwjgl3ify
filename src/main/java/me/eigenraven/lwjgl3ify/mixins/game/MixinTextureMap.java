c1b349d86d17a3da6da742ae4aa95529d63a98b8
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2023-02-02 14:00:45 -0500

    Improve performance of texture loading and stitching by using STB (#14)
    * Replace texture loading and stitching with STB
    
    * Fix mixin sidedness
    
    * Apply changes from code review
    
    * Apply spotless

---
+++ src/main/java/me/eigenraven/lwjgl3ify/mixins/game/MixinTextureMap.java
@@ -0,0 +1,29 @@
+package me.eigenraven.lwjgl3ify.mixins.game;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import me.eigenraven.lwjgl3ify.textures.NativeBackedImage;
+import net.minecraft.client.renderer.texture.AbstractTexture;
+import net.minecraft.client.renderer.texture.TextureMap;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(TextureMap.class)
+public abstract class MixinTextureMap extends AbstractTexture {
+    @Redirect(
+            method = "loadTextureAtlas",
+            at =
+                    @At(
+                            value = "INVOKE",
+                            target = "Ljavax/imageio/ImageIO;read(Ljava/io/InputStream;)Ljava/awt/image/BufferedImage;",
+                            remap = false))
+    private BufferedImage redirectImageRead(InputStream stream) {
+        try {
+            return NativeBackedImage.make(stream);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
