b1a633fea4e14b98cd9b74b78d21d39b2504d821
Raven Szewczyk <git@eigenraven.me>
2023-02-05 18:10:38 +0000

    Also catch java interface method references in the CPIMRF asm transformer
    Closes #15

--- src/main/java/me/eigenraven/lwjgl3ify/core/FixConstantPoolInterfaceMethodRefHelper.java
+++ src/main/java/me/eigenraven/lwjgl3ify/core/FixConstantPoolInterfaceMethodRefHelper.java
@@ -2,6 +2,8 @@ package me.eigenraven.lwjgl3ify.core;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import me.eigenraven.lwjgl3ify.Lwjgl3ify;
+
 import org.objectweb.asm.Handle;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AbstractInsnNode;
@@ -24,16 +26,14 @@ public class FixConstantPoolInterfaceMethodRefHelper {
         if (System.getProperty("java.specification.version", "1.8").trim().startsWith("1.8")) {
             return false;
         }
-        if ((node.access & Opcodes.ACC_INTERFACE) == 0) {
-            return false;
-        }
+        final boolean iAmAnInterface = ((node.access & Opcodes.ACC_INTERFACE) != 0);
         boolean changesMade = false;
         final String internalClassName = node.name;
         if (node.methods != null) {
             for (MethodNode method : node.methods) {
                 if (method.instructions != null) {
                     for (AbstractInsnNode insn : method.instructions) {
-                        changesMade |= validateInstruction(internalClassName, insn);
+                        changesMade |= validateInstruction(internalClassName, iAmAnInterface, insn);
                     }
                 }
             }
@@ -41,17 +41,17 @@ public class FixConstantPoolInterfaceMethodRefHelper {
         return changesMade;
     }
 
-    private boolean validateInstruction(String internalClassName, AbstractInsnNode rawInsn) {
+    private boolean validateInstruction(String internalClassName, boolean iAmAnInterface, AbstractInsnNode rawInsn) {
         AtomicBoolean changed = new AtomicBoolean(false);
         switch (rawInsn.getType()) {
             case AbstractInsnNode.INVOKE_DYNAMIC_INSN -> {
                 final InvokeDynamicInsnNode insn = (InvokeDynamicInsnNode) rawInsn;
-                insn.bsm = fixHandle(internalClassName, insn.bsm, changed);
+                insn.bsm = fixHandle(internalClassName, iAmAnInterface, insn.bsm, changed);
                 if (insn.bsmArgs != null) {
                     for (int i = 0; i < insn.bsmArgs.length; i++) {
                         final Object arg = insn.bsmArgs[i];
-                        if (arg instanceof Handle) {
-                            insn.bsmArgs[i] = fixHandle(internalClassName, (Handle) arg, changed);
+                        if (arg instanceof Handle handle) {
+                            insn.bsmArgs[i] = fixHandle(internalClassName, iAmAnInterface, handle, changed);
                         }
                     }
                 }
@@ -62,10 +62,26 @@ public class FixConstantPoolInterfaceMethodRefHelper {
         return changed.get();
     }
 
-    private Handle fixHandle(String internalClassName, Handle handle, AtomicBoolean changed) {
-        if (!handle.isInterface() && handle.getOwner().equals(internalClassName)) {
-            changed.set(true);
-            return new Handle(handle.getTag(), handle.getOwner(), handle.getName(), handle.getDesc(), true);
+    private Handle fixHandle(String internalClassName, boolean iAmAnInterface, Handle handle, AtomicBoolean changed) {
+        if (!handle.isInterface()) {
+            final boolean fixSelfReference = handle.getOwner().equals(internalClassName) && iAmAnInterface;
+            boolean fixJavaReference = false;
+            if (!fixSelfReference && handle.getOwner().startsWith("java/")) {
+                final String regularName = handle.getOwner().replace('/', '.');
+                try {
+                    final Class<?> javaClass = Class.forName(regularName);
+                    if (javaClass.isInterface()) {
+                        fixJavaReference = true;
+                    }
+                } catch (ClassNotFoundException cnfe) {
+                    // no-op
+                    Lwjgl3ify.LOG.warn("Reference to non-existing java class {} found.", regularName, cnfe);
+                }
+            }
+            if (fixSelfReference || fixJavaReference) {
+                changed.set(true);
+                return new Handle(handle.getTag(), handle.getOwner(), handle.getName(), handle.getDesc(), true);
+            }
         }
         return handle;
     }
