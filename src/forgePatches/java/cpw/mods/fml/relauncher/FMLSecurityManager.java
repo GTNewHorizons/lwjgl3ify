81c13b7b0bf533cb8d0de77df2782e0e52aac144
Raven Szewczyk <git@eigenraven.me>
2023-03-04 21:06:48 +0000

    Add an error when running with older Java 17 versions.
    ServerLaunchWrapper copy to stop the security error from happening before our error is shown.

--- src/forgePatches/java/cpw/mods/fml/relauncher/FMLSecurityManager.java
+++ src/forgePatches/java/cpw/mods/fml/relauncher/FMLSecurityManager.java
@@ -1,6 +1,12 @@
 package cpw.mods.fml.relauncher;
 
+import java.awt.*;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.security.Permission;
+
+import javax.swing.*;
 
 /**
  * A custom security manager stopping certain events from happening unexpectedly.
@@ -11,6 +17,49 @@ import java.security.Permission;
  *
  */
 public class FMLSecurityManager extends SecurityManager {
+
+    public FMLSecurityManager() {
+        // This is the first class that gets loaded, do the 17 check first.
+        if (!Boolean.getBoolean("lwjgl3ify.skipjavacheck")) {
+            final String specVer = System.getProperty("java.specification.version");
+            if (specVer.equals("17")) {
+                try {
+                    final Class<?> cRuntime = Class.forName("java.lang.Runtime");
+                    final Class<?> cRuntimeVersion = Class.forName("java.lang.Runtime$Version");
+                    final MethodHandles.Lookup lookup = MethodHandles.publicLookup();
+                    final MethodHandle mRuntimeVersion = lookup
+                            .findStatic(cRuntime, "version", MethodType.methodType(cRuntimeVersion));
+                    final MethodHandle mVersionParse = lookup
+                            .findStatic(cRuntimeVersion, "parse", MethodType.methodType(cRuntimeVersion, String.class));
+                    final MethodHandle mVersionCompareTo = lookup.findVirtual(
+                            cRuntimeVersion,
+                            "compareToIgnoreOptional",
+                            MethodType.methodType(int.class, cRuntimeVersion));
+                    final Object runtimeVersion = mRuntimeVersion.invoke();
+                    final Object ver17_0_6 = mVersionParse.invoke("17.0.6");
+                    final int cmpResult = (int) mVersionCompareTo.invoke(runtimeVersion, ver17_0_6);
+                    if (cmpResult < 0) {
+                        if (GraphicsEnvironment.isHeadless()) {
+                            System.err.println("=================================================================");
+                            System.err.println("Upgrade your Java 17 install to Java 17.0.6 for lwjgl3ify to work");
+                            System.err.println("Your Java version is: " + runtimeVersion);
+                            System.err.println("=================================================================");
+                        } else {
+                            JOptionPane.showMessageDialog(
+                                    null,
+                                    "Upgrade your Java 17 install to Java 17.0.6 for lwjgl3ify to work\nYour java version is: "
+                                            + runtimeVersion,
+                                    "Java version requirement not met",
+                                    JOptionPane.ERROR_MESSAGE);
+                        }
+                        System.exit(1);
+                    }
+                } catch (Throwable e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 
     @Override
     public void checkPermission(Permission perm) {
