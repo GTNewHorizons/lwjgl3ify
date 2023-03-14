839969de4e7087614ab6a52477425dbf0cd2f30d
Raven Szewczyk <git@eigenraven.me>
2023-03-14 13:20:10 +0000

    Add retries with larger atlases to the stitcher code
    Closes #31

--- src/main/java/me/eigenraven/lwjgl3ify/textures/StbStitcher.java
+++ src/main/java/me/eigenraven/lwjgl3ify/textures/StbStitcher.java
@@ -7,6 +7,7 @@ import net.minecraft.client.renderer.StitcherException;
 import net.minecraft.client.renderer.texture.Stitcher;
 import net.minecraft.util.MathHelper;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.lwjgl.stb.STBRPContext;
 import org.lwjgl.stb.STBRPNode;
 import org.lwjgl.stb.STBRPRect;
@@ -19,7 +20,7 @@ import cpw.mods.fml.common.ProgressManager;
 public class StbStitcher {
 
     // Returns size
-    public static int packRects(Stitcher.Holder[] holders) {
+    public static Pair<Integer, Integer> packRects(Stitcher.Holder[] holders) {
         ProgressManager.ProgressBar bar = ProgressManager.push("Stitch setup", (holders.length + 99) / 100);
         int holderSize = holders.length;
 
@@ -29,6 +30,7 @@ public class StbStitcher {
             // Initialize the rectangles that we'll be using in the calculation
             // While that's happening, sum up the area needed to fit all of the images
             int sqSize = 0;
+            int maxW = 16, maxH = 16;
             for (int j = 0; j < holderSize; ++j) {
                 Stitcher.Holder holder = holders[j];
                 if ((j % 100) == 0 && bar.getStep() < bar.getSteps()) {
@@ -41,48 +43,66 @@ public class StbStitcher {
                 // The ID here is just the array index, for easy lookup later
                 rectBuf.get(j).set(j, width, height, 0, 0, false);
 
-                sqSize += (width * height);
+                sqSize += width * height;
+                maxW = Math.max(maxW, width);
+                maxH = Math.max(maxH, height);
             }
 
-            int size = MathHelper.roundUpToPowerOfTwo((int) Math.sqrt(sqSize));
-
-            // TODO: if sqSize < (size * size) / 2, then we can use size / 2 for the height to save VRAM
+            maxW = MathHelper.roundUpToPowerOfTwo(maxW);
+            maxH = MathHelper.roundUpToPowerOfTwo(maxH);
+            final int guessedSqSide = MathHelper.roundUpToPowerOfTwo((int) Math.ceil(Math.sqrt(sqSize)));
+            int atlasWidth = Math.max(guessedSqSide, maxW * 2);
+            int atlasHeight = Math.max(guessedSqSide, maxH * 2);
 
             // Internal node structure needed for STB
-            try (STBRPNode.Buffer nodes = STBRPNode.malloc(size + 1)) {
-                // Initialize the rect packer
-                STBRectPack.stbrp_init_target(ctx, size, size, nodes);
+            boolean doubleX = true;
+            for (int retries = 0; retries < 10; retries++) {
+                try (STBRPNode.Buffer nodes = STBRPNode.malloc(atlasWidth + 32)) {
+                    // Initialize the rect packer
+                    STBRectPack.stbrp_init_target(ctx, atlasWidth, atlasHeight, nodes);
 
-                // Perform rectangle packing
-                STBRectPack.stbrp_pack_rects(ctx, rectBuf);
-
-                for (STBRPRect rect : rectBuf) {
-                    Stitcher.Holder holder = holders[rect.id()];
-
-                    // Ensure that everything is properly packed!
-                    if (!rect.was_packed()) {
-                        throw new StitcherException(
-                                holder,
-                                "Could not fit " + holder.getAtlasSprite()
-                                        .getIconName() + " into " + size + "x" + size + " atlas!");
+                    // Perform rectangle packing
+                    if (STBRectPack.stbrp_pack_rects(ctx, rectBuf) == 0) {
+                        // Not everything was packed
+                        if (doubleX) {
+                            atlasWidth *= 2;
+                        } else {
+                            atlasHeight *= 2;
+                        }
+                        doubleX = !doubleX;
+                        continue;
                     }
 
-                    // Initialize the sprite now with the position and size that we've calculated so far
+                    for (STBRPRect rect : rectBuf) {
+                        Stitcher.Holder holder = holders[rect.id()];
 
-                    // texture should not be rotated, so use false
-                    holder.getAtlasSprite().initSprite(size, size, rect.x(), rect.y(), false);
+                        // Ensure that everything is properly packed!
+                        if (!rect.was_packed()) {
+                            throw new StitcherException(
+                                    holder,
+                                    "Could not fit " + holder.getAtlasSprite()
+                                            .getIconName() + " into " + atlasWidth + "x" + atlasHeight + " atlas!");
+                        }
+
+                        // Initialize the sprite now with the position and size that we've calculated so far
+
+                        // texture should not be rotated, so use false
+                        holder.getAtlasSprite().initSprite(atlasWidth, atlasHeight, rect.x(), rect.y(), false);
+                    }
+
+                    // Safeguard in case our calculation was off
+                    while (bar.getStep() < bar.getSteps()) {
+                        Lwjgl3ify.LOG.warn("Progressbar calculation was off");
+                        bar.step("noop");
+                    }
+
+                    ProgressManager.pop(bar);
+
+                    return Pair.of(atlasWidth, atlasHeight);
                 }
-
-                // Safeguard in case our calculation was off
-                while (bar.getStep() < bar.getSteps()) {
-                    Lwjgl3ify.LOG.warn("Progressbar calculation was off");
-                    bar.step("noop");
-                }
-
-                ProgressManager.pop(bar);
-
-                return size;
             }
+            throw new IllegalStateException(
+                    "Could not fit all sprites into an atlas of size " + atlasWidth + "x" + atlasHeight);
         }
     }
 }
