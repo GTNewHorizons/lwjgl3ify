5233fafe335786e8f9ba297af01037596d552dc5
Raven Szewczyk <git@eigenraven.me>
2024-01-21 21:22:57 +0000

    Start work on RFB bundling/support

--- src/main/java/me/eigenraven/lwjgl3ify/core/Lwjgl3ifyCoremod.java
+++ src/main/java/me/eigenraven/lwjgl3ify/core/Lwjgl3ifyCoremod.java
@@ -34,16 +34,6 @@ public class Lwjgl3ifyCoremod implements IFMLLoadingPlugin, IEarlyMixinLoader {
         Config.loadConfig();
         try {
             LaunchClassLoader launchLoader = (LaunchClassLoader) getClass().getClassLoader();
-            // Packages that used to be in rt.jar
-            launchLoader.addClassLoaderExclusion("com.sun");
-            launchLoader.addClassLoaderExclusion("com.oracle");
-            launchLoader.addClassLoaderExclusion("javax");
-            launchLoader.addClassLoaderExclusion("jdk");
-            launchLoader.addClassLoaderExclusion("org.ietf.jgss");
-            launchLoader.addClassLoaderExclusion("org.jcp.xml.dsig.internal");
-            launchLoader.addClassLoaderExclusion("org.omg");
-            launchLoader.addClassLoaderExclusion("org.w3c.dom");
-            launchLoader.addClassLoaderExclusion("org.xml.sax");
             launchLoader.addClassLoaderExclusion("org.hotswap.agent");
             launchLoader.addClassLoaderExclusion("org.lwjglx.debug");
         } catch (ClassCastException e) {
