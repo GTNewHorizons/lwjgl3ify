8f12d27bce56cc962a656f5c5b63b4c8a9369876
Raven Szewczyk <git@eigenraven.me>
2024-02-18 11:12:23 +0000

    Add OSX and Windows JVM installation scanning

--- src/main/java/me/eigenraven/lwjgl3ify/relauncher/JvmLocator.java
+++ src/main/java/me/eigenraven/lwjgl3ify/relauncher/JvmLocator.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.commons.io.IOUtils;
@@ -38,7 +41,27 @@ public final class JvmLocator {
         }
 
         if (SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_FREE_BSD) {
-            searchLinuxDirs(out);
+            try {
+                searchLinuxDirs(out);
+            } catch (Throwable t) {
+                Relauncher.logger.warn("Could not scan JVM homes", t);
+            }
+        }
+
+        if (SystemUtils.IS_OS_MAC) {
+            try {
+                searchMacJavaHomes(out);
+            } catch (Throwable t) {
+                Relauncher.logger.warn("Could not scan JVM homes", t);
+            }
+        }
+
+        if (SystemUtils.IS_OS_WINDOWS) {
+            try {
+                searchWindowsRegistry(out);
+            } catch (Throwable t) {
+                Relauncher.logger.warn("Could not scan JVM homes", t);
+            }
         }
 
         Collections.sort(out);
@@ -82,13 +105,115 @@ public final class JvmLocator {
         }
     }
 
+    private static final Pattern OSX_INSTALLATION_PATTERN = Pattern.compile(".+\\s+(/.+)");
+
+    private static void searchMacJavaHomes(List<Path> out) {
+        final String[] cmdOut = getProcessOutput("/usr/libexec/java_home", "-V").split("\n");
+        for (final String outLine : cmdOut) {
+            final Matcher matcher = OSX_INSTALLATION_PATTERN.matcher(outLine);
+            if (matcher.matches()) {
+                final String javaHome = matcher.group(1);
+                final Path binJava = BIN_JAVA_RESOLVER.apply(Paths.get(javaHome));
+                if (Files.isRegularFile(binJava)) {
+                    out.add(binJava);
+                }
+            }
+        }
+    }
+
+    private static void searchWindowsRegistry(List<Path> out) {
+        // OpenJDK candidates
+        final List<String> javaHomes = new ArrayList<>();
+        Stream.of("SOFTWARE\\AdoptOpenJDK\\JDK", "SOFTWARE\\Eclipse Adoptium\\JDK", "SOFTWARE\\Eclipse Foundation\\JDK")
+            .forEach(p -> javaHomes.addAll(scanRegistryJvms(p, "\\hotspot\\MSI", "Path")));
+        // Other JDKs
+        Stream
+            .of(
+                "SOFTWARE\\JavaSoft\\JDK",
+                "SOFTWARE\\JavaSoft\\Java Development Kit",
+                "SOFTWARE\\JavaSoft\\Java Runtime Environment",
+                "SOFTWARE\\Wow6432Node\\JavaSoft\\Java Development Kit",
+                "SOFTWARE\\Wow6432Node\\JavaSoft\\Java Runtime Environment")
+            .forEach(p -> javaHomes.addAll(scanRegistryJvms(p, "", "JavaHome")));
+        for (final String javaHome : javaHomes) {
+            final Path binJava = BIN_JAVA_RESOLVER.apply(Paths.get(javaHome));
+            if (Files.isRegularFile(binJava)) {
+                out.add(binJava);
+            }
+        }
+    }
+
+    private static List<String> scanRegistryJvms(String sdkSubkey, String path, String value) {
+        return getRegistrySubkeys("HKLM\\" + sdkSubkey).stream()
+            .map(withVersion -> getRegistryKey(withVersion + path, value))
+            .collect(Collectors.toList());
+    }
+
+    private static String getRegistryKey(String location, String key) {
+        final String out = getProcessOutput("REG.EXE", "QUERY", location, "/v", key);
+        if (out.isEmpty()) {
+            return null;
+        }
+        if (!out.contains(key)) {
+            return null;
+        }
+        for (String line : out.split("\n")) {
+            // key[spaces]REG_type[spaces]value
+            line = line.trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            if (!line.startsWith(key)) {
+                continue;
+            }
+            line = line.substring(key.length())
+                .trim();
+            final String firstWord = line.split("\\s+")[0];
+            line = line.substring(firstWord.length())
+                .trim();
+            return line;
+        }
+        return null;
+    }
+
+    // HKCU\a -> HKCU\a\b, HKCU\a\c, ...
+    private static List<String> getRegistrySubkeys(String location) {
+        final String out = getProcessOutput("REG.EXE", "QUERY", location);
+        if (out.isEmpty()) {
+            return Collections.emptyList();
+        }
+        final String sublocationPrefix = location + '\\';
+        if (!out.contains(sublocationPrefix)) {
+            return Collections.emptyList();
+        }
+        final List<String> ret = new ArrayList<>();
+        for (String line : out.split("\n")) {
+            // key[spaces]REG_type[spaces]value
+            line = line.trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            if (!line.startsWith(sublocationPrefix)) {
+                continue;
+            }
+            ret.add(line);
+        }
+        return ret;
+    }
+
     private static String getProcessOutput(String... cmd) {
         try {
-            final Process p = new ProcessBuilder(cmd).redirectOutput(ProcessBuilder.Redirect.PIPE)
-                .start();
-            String stdout = IOUtils.toString(p.getInputStream());
-            p.waitFor();
-            return stdout;
+            final ProcessBuilder pb = new ProcessBuilder(cmd).redirectOutput(ProcessBuilder.Redirect.PIPE)
+                .redirectError(ProcessBuilder.Redirect.PIPE);
+            pb.environment()
+                .remove("JAVA_VERSION");
+            final Process p = pb.start();
+            String stdout = IOUtils.toString(p.getInputStream()) + "\n" + IOUtils.toString(p.getErrorStream());
+            final int ec = p.waitFor();
+            if (ec != 0) {
+                return "";
+            }
+            return stdout.trim();
         } catch (IOException | InterruptedException e) {
             return "";
         }
