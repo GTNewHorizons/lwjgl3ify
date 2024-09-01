b0a2004de484a58eae05e2e10448fe272164b293
James R Larrowe <larrowe.semaj11@gmail.com>
2024-09-01 14:23:34 -0400

    Fix getPixelScaleFactor
    Previous implementation was broken on hidpi platforms where the
    size of the framebuffer and the size of the window correlate 1:1.
    This manifested in the mouse code thinking the mouse was in a
    different location than it actually was.
    
    New implementation explicitly calculates the ratio between the
    framebuffer size and the window size, rather than relying on
    glfwGetWindowContentScale.
    
    Remove GLFW_SCALE_FRAMEBUFFER/GLFW_COCOA_RETINA_FRAMEBUFFER,
    it appears that it is not needed.

--- src/main/java/org/lwjglx/opengl/Display.java
+++ src/main/java/org/lwjglx/opengl/Display.java
@@ -195,9 +195,6 @@ public class Display {
         glfwWindowHintString(GLFW_X11_CLASS_NAME, Config.X11_CLASS_NAME);
         glfwWindowHintString(GLFW_COCOA_FRAME_NAME, Config.COCOA_FRAME_NAME);
 
-        // request a non-hidpi framebuffer on Retina displays on MacOS
-        if (glfwGetPlatform() == GLFW_PLATFORM_COCOA) glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_FALSE);
-
         if (Config.WINDOW_CENTERED) {
             glfwWindowHint(GLFW_POSITION_X, (monitorWidth - mode.getWidth()) / 2);
             glfwWindowHint(GLFW_POSITION_Y, (monitorHeight - mode.getHeight()) / 2);
@@ -887,10 +884,20 @@ public class Display {
         if (!isCreated()) {
             return 1.0f;
         }
-        float[] xScale = new float[1];
-        float[] yScale = new float[1];
-        glfwGetWindowContentScale(getWindow(), xScale, yScale);
-        return Math.max(xScale[0], yScale[0]);
+        int[] windowWidth = new int[1];
+        int[] windowHeight = new int[1];
+        int[] framebufferWidth = new int[1];
+        int[] framebufferHeight = new int[1];
+        float xScale, yScale;
+        // via technicality we actually have to divide the framebuffer
+        // size by the window size here, since glfwGetWindowContentScale
+        // returns a value not equal to 1 even on platforms where the
+        // framebuffer size and window size always map 1:1
+        glfwGetWindowSize(getWindow(), windowWidth, windowHeight);
+        glfwGetFramebufferSize(getWindow(), framebufferWidth, framebufferHeight);
+        xScale = (float)framebufferWidth[0]/windowWidth[0];
+        yScale = (float)framebufferHeight[0]/windowHeight[0];
+        return Math.max(xScale, yScale);
     }
 
     public static void setSwapInterval(int value) {
