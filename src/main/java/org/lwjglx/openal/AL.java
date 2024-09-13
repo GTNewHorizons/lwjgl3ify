9c10c0396113649144d80804fb6df6eb2e1edd65
kstvr32 <109012629+kstvr32@users.noreply.github.com>
2024-09-13 13:51:28 -0400

    add null check to mimic behavior of lwjgl2 (#173)
    fix error type
    
    Co-authored-by: kstvr32 <kstvr32@gmail.com>

--- src/main/java/org/lwjglx/openal/AL.java
+++ src/main/java/org/lwjglx/openal/AL.java
@@ -26,12 +26,12 @@ public class AL {
     }
 
     public static void create(String deviceArguments, int contextFrequency, int contextRefresh,
-        boolean contextSynchronized) {
+        boolean contextSynchronized) throws LWJGLException {
         create(deviceArguments, contextFrequency, contextRefresh, contextSynchronized, true);
     }
 
     public static void create(String deviceArguments, int contextFrequency, int contextRefresh,
-        boolean contextSynchronized, boolean openDevice) {
+        boolean contextSynchronized, boolean openDevice) throws LWJGLException {
         IntBuffer attribs = BufferUtils.createIntBuffer(16);
 
         attribs.put(org.lwjgl.openal.ALC10.ALC_FREQUENCY);
@@ -63,6 +63,10 @@ public class AL {
         String defaultDevice = org.lwjgl.openal.ALC10.alcGetString(0, ALC10.ALC_DEFAULT_DEVICE_SPECIFIER);
 
         long deviceHandle = org.lwjgl.openal.ALC10.alcOpenDevice(defaultDevice);
+
+        if (deviceHandle == 0) {
+            throw new LWJGLException("Could not open ALC device");
+        }
 
         alcDevice = new ALCdevice(deviceHandle);
         final ALCCapabilities deviceCaps = org.lwjgl.openal.ALC.createCapabilities(deviceHandle);
