55377a8234e9737dad896861f1329062032beb29
QuanhuZeYu <160235071+QuanhuZeYu@users.noreply.github.com>
2025-06-07 19:44:47 +0800

    Fix UTF-32 codepoint input handling (#222)

---
+++ src/main/java/me/eigenraven/lwjgl3ify/mixins/early/game/MixinGuiScreenKeyTypeInput.java
@@ -0,0 +1,33 @@
+package me.eigenraven.lwjgl3ify.mixins.early.game;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+
+import org.lwjgl.input.Keyboard;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GuiScreen.class)
+public abstract class MixinGuiScreenKeyTypeInput {
+
+    @Shadow
+    protected abstract void keyTyped(char c, int keycode);
+
+    @Inject(method = "handleKeyboardInput", at = @At("HEAD"), cancellable = true)
+    private void lwjgl3ify$handleKeyboardInput(CallbackInfo ci) {
+        if (Keyboard.getEventKeyState()) {
+            int codepoint = org.lwjglx.input.Keyboard.lwjgl3ify$getEventCodePoint();
+            int keycode = Keyboard.getEventKey();
+            char[] chars = Character.toChars(codepoint);
+            for (char c : chars) {
+                keyTyped(c, keycode);
+            }
+        }
+        Minecraft.getMinecraft()
+            .func_152348_aa();
+        ci.cancel();
+    }
+}
