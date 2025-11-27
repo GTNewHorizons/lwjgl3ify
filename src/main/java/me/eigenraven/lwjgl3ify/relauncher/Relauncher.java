316e498c533ba06ae0357b703744d107856c8fc5
Raven Szewczyk <git@eigenraven.me>
2025-11-27 17:49:39 +0000

    relauncher: Download to localappdata, not temp

--- src/main/java/me/eigenraven/lwjgl3ify/relauncher/Relauncher.java
+++ src/main/java/me/eigenraven/lwjgl3ify/relauncher/Relauncher.java
@@ -61,14 +61,11 @@ public class Relauncher {
         Path cacheDir;
         try {
             if (system.contains("win")) {
-                String temp = System.getenv("TEMP");
                 String localAppData = System.getenv("LOCALAPPDATA");
-                if (temp != null) {
-                    cacheDir = Paths.get(temp, "lwjgl3ify");
-                } else if (localAppData != null) {
-                    cacheDir = Paths.get(localAppData, "Temp", "lwjgl3ify");
+                if (localAppData != null) {
+                    cacheDir = Paths.get(localAppData, "lwjgl3ify");
                 } else {
-                    cacheDir = Paths.get(userHome, "AppData", "Local", "Temp", "lwjgl3ify");
+                    cacheDir = Paths.get(userHome, "AppData", "Local", "lwjgl3ify");
                 }
             } else if (system.contains("mac")) {
                 cacheDir = Paths.get(userHome, "Library", "Caches", "lwjgl3ify");
@@ -82,7 +79,7 @@ public class Relauncher {
             }
         } catch (InvalidPathException e) {
             if (system.contains("win")) {
-                cacheDir = Paths.get(userHome, "AppData", "Local", "Temp", "lwjgl3ify");
+                cacheDir = Paths.get(userHome, "AppData", "Local", "lwjgl3ify");
             } else {
                 cacheDir = Paths.get(userHome, ".cache", "lwjgl3ify");
             }
