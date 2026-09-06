aa601e04db516671c8e50f42a7b9991ca928a180
Algent <algent@gmail.com>
2026-09-06 13:11:40 +0200

    fix(openal): honor requested output device

--- src/main/java/org/lwjglx/openal/AL.java
+++ src/main/java/org/lwjglx/openal/AL.java
@@ -4,7 +4,6 @@ import static org.lwjgl.system.MemoryStack.stackPush;
 
 import java.nio.IntBuffer;
 
-import org.lwjgl.openal.ALC10;
 import org.lwjgl.openal.ALCCapabilities;
 import org.lwjgl.system.MemoryStack;
 import org.lwjglx.LWJGLException;
@@ -63,9 +62,7 @@ public class AL {
             attribs.put(0);
             attribs.flip();
 
-            String defaultDevice = org.lwjgl.openal.ALC10.alcGetString(0, ALC10.ALC_DEFAULT_DEVICE_SPECIFIER);
-
-            long deviceHandle = org.lwjgl.openal.ALC10.alcOpenDevice(defaultDevice);
+            long deviceHandle = org.lwjgl.openal.ALC10.alcOpenDevice(deviceArguments);
 
             if (deviceHandle == 0) {
                 throw new LWJGLException("Could not open ALC device");
